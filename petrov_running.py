f5fa83079ce67e4291539bd66cb85a4b96d576da
taflynn <t.flynn@ncl.ac.uk>
2021-06-08 16:38:15 +0100

    producing data for APR

--- petrov_running.py
+++ petrov_running.py
@@ -19,26 +19,26 @@ from freq_funcs import curve_fitting # curve fitting function
 
 # SWITCHES:
 # trap? 0=NO,1=YES
-trap = 1
+trap = 0
 # interacting gas? (0 = Non-interacting gas, 1 = Interacting gas w/ LHY)
 int_gas = 1
 # breathing mode? (0 = No initial phase perturbation, hence no oscillation, 1 = Small phase perturbation)
 mode = 1
 
 # GRID
-Lr = 32 # box length
+Lr = 12 # box length
 Nr = 256 # grid points
 dr = Lr/Nr # spatial step
 r = np.arange(-1/2,(Nr + 3/2),1)*dr # position array with 4 ghost points
 
 # TIME SETUP
 dt = 0.1*dr**2 # time step 
-im_t_steps = 250000 # number of imaginary time steps
-t_steps = 30000 # number of real time steps (do not go too high as curve fitting can break down)
+im_t_steps = 250000 #250000# number of imaginary time steps
+t_steps = 0#30000 # number of real time steps (do not go too high as curve fitting can break down)
 
 # PARAMETERS
 pi = np.math.pi
-N = 3000
+N = 15
 a = 0.05
 
 # POTENTIAL
@@ -60,11 +60,13 @@ phi_ground = phi
 [phi,spacetime,t_array,mean_r2]	= petrov_real_tim_rk4_mat(phi,mu,r,dr,dt,N,0.99*V,int_gas,t_steps,mode)
 
 # SAVE FIGURE ON THE ROOT PROCESS
+"""
 plt.plot(t_array,mean_r2)
 plt.xlim(t_array[0],t_array[-1])
 plt.xlabel("$t$")
 plt.ylabel("$<r^2>$")
 plt.savefig("breath_N"+str(N)+"a"+str(a)+".png",dpi=300)
+"""
 """
 plt.plot(r,N*np.abs(phi_ground)**2)
 plt.xlim(r[0],r[-1])
@@ -73,6 +75,7 @@ plt.xlabel("$r$")
 plt.ylabel("$n_0$")
 plt.savefig("ground_trapped_a"+str(a)+".png",dpi=300)
 plt.clf()
+"""
 """
 R,T = np.meshgrid(r,t_array)
 plt.figure(figsize=(12,10))
@@ -84,11 +87,11 @@ plt.ylabel("t")
 plt.title("Density of $\phi(r,t)$")
 plt.savefig('spacetime_breath_N'+str(N)+'a'+str(a)+".png",dpi=300)
 """
+"""
 # SAVING DATA 
 obs_data = np.column_stack((t_array,mean_r2))
 np.savetxt('obs_quench.csv',obs_data,delimiter=',',fmt='%18.16f')
 """
-"""
+
 ground_data = np.column_stack((r,np.sqrt(N)*phi_ground))
-np.savetxt('phi0_N'+str(N)+'a'+str(a)+'.csv',ground_data,delimiter=',',fmt='%18.16f')
-"""
+np.savetxt('phi0_N'+str(N)+'.csv',ground_data,delimiter=',',fmt='%18.16f')
